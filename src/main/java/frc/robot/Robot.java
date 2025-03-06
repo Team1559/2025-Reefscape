35b95aac9d33fd939a864feb24a1bcbe08f21701
SWDeviltech <SWDeviltech@gmail.com>
2025-03-05 21:32:33 -0500

    partial refactoring of command binding

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -20,10 +20,12 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.StartEndCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.lib.subsystems.swerve.TeleopDriveCommand;
 import frc.robot.commands.AlgaeIntakeAngleCommand;
+import frc.robot.commands.CoralIntakeAngleCommand;
 import frc.robot.commands.ElevatorHeightCommand2025;
 import frc.robot.subsystems.Elevator2025;
 import frc.robot.subsystems.Elevator2025.Level;
@@ -57,7 +59,7 @@ public class Robot extends LoggedRobot {
         coPilotController = new CommandXboxController(1);
 
         drivetrain = new SwerveDrive2025();
-      
+
         vision = new Vision2025(drivetrain);
         elevator = new Elevator2025();
         coralIntake = new CoralIntake();
@@ -68,67 +70,108 @@ public class Robot extends LoggedRobot {
         DriverStation.silenceJoystickConnectionWarning(true);
     }
 
-    public void clearControllers(){
+    public void clearControllers() {
         CommandScheduler.getInstance().getActiveButtonLoop().clear();
+    }
+
+    public void makeCommands() {
+        Command elevatorL1 = new ElevatorHeightCommand2025(elevator, Level.L1_CORAL);
+        Command elevatorL2 = new ElevatorHeightCommand2025(elevator, Level.L2_CORAL);
+        Command elevatorL3 = new ElevatorHeightCommand2025(elevator, Level.L3_CORAL);
+        Command elevatorL4 = new ElevatorHeightCommand2025(elevator, Level.L4_CORAL);
+        NamedCommands.registerCommand("elevatorL1", elevatorL1);
+        NamedCommands.registerCommand("elevatorL2", elevatorL2);
+        NamedCommands.registerCommand("elevatorL3", elevatorL3);
+        NamedCommands.registerCommand("elevatorL4", elevatorL4);
+
+        Command coralAngleFeeder = new CoralIntakeAngleCommand(coralIntake, CoralIntake.TargetAngle.SOURCE_ANGLE);
+        Command coralAngleL2 = new CoralIntakeAngleCommand(coralIntake, CoralIntake.TargetAngle.L2_ANGLE);
+        Command coralAngleL3 = new CoralIntakeAngleCommand(coralIntake, CoralIntake.TargetAngle.L2_ANGLE);
+        Command coralAngleL4 = new CoralIntakeAngleCommand(coralIntake, CoralIntake.TargetAngle.L2_ANGLE);
+        
+        Command algaeLevelTwo = new ElevatorHeightCommand2025(elevator, Level.L2_ALGAE);
+        Command algaeLevelThree = new ElevatorHeightCommand2025(elevator, Level.L3_ALGAE);
+        NamedCommands.registerCommand("algaeLevel2", algaeLevelTwo);
+        NamedCommands.registerCommand("algaeLevel3", algaeLevelThree);
+
+        Command stowAlgae = new AlgaeIntakeAngleCommand(algaeIntake, AlgaeIntake.TargetAngle.STOWED);
+
+        Command coralAlignL2 = new SequentialCommandGroup(stowAlgae, elevatorL2, coralAngleL2);
+        Command coralAlignL3 = new SequentialCommandGroup(stowAlgae, elevatorL3, coralAngleL3);
+        Command coralAlignL4 = new SequentialCommandGroup(stowAlgae, elevatorL4, coralAngleL4);
+
+        NamedCommands.registerCommand("coralAlignL2", coralAlignL2);
+        NamedCommands.registerCommand("coralAlignL3", coralAlignL3);
+        NamedCommands.registerCommand("coralAlignL4", coralAlignL4);
+
+        Command coralIn = new StartEndCommand(() -> coralIntake.run(false), () -> coralIntake.stop(), coralIntake);
+        Command coralOut = new StartEndCommand(() -> coralIntake.run(true), () -> coralIntake.stop(), coralIntake);
+        // TODO: verify in/out
+
+        Command elevatorHome = new InstantCommand(
+                () -> elevator.goHome(),
+                elevator);
+        NamedCommands.registerCommand("elevatorHome", elevatorHome);
+
+        Command manualElevatorUp = new InstantCommand(
+                () -> elevator.changeTargetPosition(.005), elevator);
+        NamedCommands.registerCommand("manualElevatorUp", manualElevatorUp);
+
+
     }
 
     public void setTeleopBindings(){
         drivetrain.setDefaultCommand(new TeleopDriveCommand(pilotController::getLeftY, pilotController::getLeftX,
                 pilotController::getRightX, SWERVE_MAX_LINEAR_VELOCITY, SWERVE_MAX_ANGULAR_VELOCITY, drivetrain));
+
+        coPilotController.b().onTrue(NamedCommands.getCommand("coralAlignL2"));
+        coPilotController.x().onTrue(NamedCommands.getCommand("coralAlignL3"));
+        coPilotController.y().onTrue(NamedCommands.getCommand("coralAlignL4"));
+
+        coPilotController.rightBumper();
+        
     }
 
-    public void setTestBindings(){
-        coPilotController.a().onTrue(NamedCommands.getCommand("coralLevelOne"));
-        coPilotController.b().and(coPilotController.rightTrigger().negate()).onTrue(NamedCommands.getCommand("coralLevelTwo"));
-        coPilotController.b().and(coPilotController.rightTrigger()).onTrue(NamedCommands.getCommand("algaeLevelTwo"));
-        coPilotController.x().and(coPilotController.rightTrigger().negate()).onTrue(NamedCommands.getCommand(("coralLevelThree")));
-        coPilotController.x().and(coPilotController.rightTrigger()).onTrue(NamedCommands.getCommand("coralLevelFour"));
+    public void setTestBindings() {
+        coPilotController.a().onTrue(NamedCommands.getCommand("elevatorL1"));
+        coPilotController.b().and(coPilotController.rightTrigger().negate())
+                .onTrue(NamedCommands.getCommand("elevatorL2"));
+        coPilotController.b().and(coPilotController.rightTrigger()).onTrue(NamedCommands.getCommand("algaeLevel2"));
+        coPilotController.x().and(coPilotController.rightTrigger().negate())
+                .onTrue(NamedCommands.getCommand(("elevatorL3")));
+        coPilotController.x().and(coPilotController.rightTrigger()).onTrue(NamedCommands.getCommand("elevatorL4"));
 
-        coPilotController.y().onTrue(NamedCommands.getCommand("coralLevelFour"));
+        coPilotController.y().onTrue(NamedCommands.getCommand("elevatorL4"));
         // coPilotController.povDown().onTrue(manualElevatorUp);//.onTrue(elevatorHome);
 
         drivetrain.setDefaultCommand(new TeleopDriveCommand(pilotController::getLeftY, pilotController::getLeftX,
-                pilotController::getRightX, 5.21, 1.925, drivetrain));
-        pilotController.a().onTrue(new InstantCommand(elevator::stop));//TODO: temp
+                pilotController::getRightX, SWERVE_MAX_LINEAR_VELOCITY, SWERVE_MAX_ANGULAR_VELOCITY, drivetrain));
+        pilotController.a().onTrue(new InstantCommand(elevator::stop));// TODO: temp
 
-        coPilotController.rightTrigger().whileTrue(new StartEndCommand(() -> algaeIntake.run(false), () -> algaeIntake.stop(), algaeIntake));
-        coPilotController.rightBumper().whileTrue(new StartEndCommand(() -> algaeIntake.run(true), () -> algaeIntake.stop(), algaeIntake));
-        
-        coPilotController.leftTrigger().whileTrue(new StartEndCommand(() -> coralIntake.run(false), () -> coralIntake.stop(), coralIntake));
-        coPilotController.leftBumper().whileTrue(new StartEndCommand(() -> coralIntake.run(true), () -> coralIntake.stop(), coralIntake));
+        coPilotController.rightTrigger()
+                .whileTrue(new StartEndCommand(() -> algaeIntake.run(false), () -> algaeIntake.stop(), algaeIntake));
+        coPilotController.rightBumper()
+                .whileTrue(new StartEndCommand(() -> algaeIntake.run(true), () -> algaeIntake.stop(), algaeIntake));
+
+        coPilotController.leftTrigger()
+                .whileTrue(new StartEndCommand(() -> coralIntake.run(false), () -> coralIntake.stop(), coralIntake));
+        coPilotController.leftBumper()
+                .whileTrue(new StartEndCommand(() -> coralIntake.run(true), () -> coralIntake.stop(), coralIntake));
 
         coPilotController.povUp().onTrue(new AlgaeIntakeAngleCommand(algaeIntake, AlgaeIntake.TargetAngle.STOWED));
         coPilotController.povDown().onTrue(new AlgaeIntakeAngleCommand(algaeIntake, AlgaeIntake.TargetAngle.REEF));
-        
-        // coPilotController.povLeft().onTrue(new CoralIntakeAngleCommand(coralIntake, CoralIntake.TargetAngle.L2_ANGLE));
-        // coPilotController.povRight().onTrue(new CoralIntakeAngleCommand(coralIntake, CoralIntake.TargetAngle.L4_ANGLE));
-        // coPilotController.povUp().onTrue(new CoralIntakeAngleCommand(coralIntake, CoralIntake.TargetAngle.SOURCE_ANGLE));
+
+        // coPilotController.povLeft().onTrue(new CoralIntakeAngleCommand(coralIntake,
+        // CoralIntake.TargetAngle.L2_ANGLE));
+        // coPilotController.povRight().onTrue(new CoralIntakeAngleCommand(coralIntake,
+        // CoralIntake.TargetAngle.L4_ANGLE));
+        // coPilotController.povUp().onTrue(new CoralIntakeAngleCommand(coralIntake,
+        // CoralIntake.TargetAngle.SOURCE_ANGLE));
     }
 
     @Override
     public void robotInit() {
-        Command coralLevelOne = new ElevatorHeightCommand2025(elevator, Level.L1_CORAL);
-        Command coralLevelTwo = new ElevatorHeightCommand2025(elevator, Level.L2_CORAL);
-        Command coralLevelThree = new ElevatorHeightCommand2025(elevator, Level.L3_CORAL);
-        Command coralLevelFour = new ElevatorHeightCommand2025(elevator, Level.L4_CORAL);
-
-        Command algaeLevelTwo = new ElevatorHeightCommand2025(elevator, Level.L2_ALGAE);
-        Command algaeLevelThree = new ElevatorHeightCommand2025(elevator, Level.L3_ALGAE);
-
-        Command elevatorHome = new InstantCommand(
-                () -> elevator.goHome(),
-                elevator);
-
-        Command manualElevatorUp = new InstantCommand(
-            () -> elevator.changeTargetPosition(.005), elevator
-        );
-        NamedCommands.registerCommand("coralLevelOne", coralLevelOne);
-        NamedCommands.registerCommand("coralLevelTwo", coralLevelTwo);
-        NamedCommands.registerCommand("coralLevelThree", coralLevelThree);
-        NamedCommands.registerCommand("coralLevelFour", coralLevelFour);
-        NamedCommands.registerCommand("algaeLevelTwo", algaeLevelTwo);
-        NamedCommands.registerCommand("algaeLevelThree", algaeLevelThree);
-        NamedCommands.registerCommand("elevatorHome", elevatorHome);
+        makeCommands();
     }
 
     @Override
