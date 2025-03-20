cd151b225d504f52e5a4609f4215dea10c768dc0
Michael Marchetti <mike@themarchettis.com>
2025-03-20 18:17:35 -0400

    add VISION_ENABLED flag

--- src/main/java/frc/robot/subsystems/Vision2025.java
+++ src/main/java/frc/robot/subsystems/Vision2025.java
@@ -9,17 +9,22 @@ import frc.lib.subsystems.vision.Vision;
 import frc.lib.subsystems.vision.VisionCameraIo;
 
 public class Vision2025 extends Vision {
+    private static final boolean VISION_ENABLED = false;
 
     public Vision2025(SwerveDrive swerveDrive) {
         super("Vision", swerveDrive, cameras(swerveDrive));
     }
 
     private static VisionCameraIo[] cameras(SwerveDrive swerveDrive) {
-        Supplier<Rotation2d> yaw = () -> swerveDrive.getPosition().getRotation();
-        // VisionCameraIo frontRight = new LimelightCameraIo("FrontRight", "limelight-frontr", yaw);
-        // VisionCameraIo frontLeft = new LimelightCameraIo("FrontLeft", "limelight-frontl", yaw);
-        // VisionCameraIo rear = new LimelightCameraIo("Rear", "limelight", yaw);
-        // VisionCameraIo frontCenter = new LimelightCameraIo("FrontCenter", "limelight-frontc", yaw);
-        return new VisionCameraIo[] {};//frontRight, rear, frontLeft, frontCenter };
+        if (VISION_ENABLED) {
+            Supplier<Rotation2d> yaw = () -> swerveDrive.getPosition().getRotation();
+            VisionCameraIo frontRight = new LimelightCameraIo("FrontRight", "limelight-frontr", yaw);
+            VisionCameraIo frontLeft = new LimelightCameraIo("FrontLeft", "limelight-frontl", yaw);
+            VisionCameraIo rear = new LimelightCameraIo("Rear", "limelight", yaw);
+            VisionCameraIo frontCenter = new LimelightCameraIo("FrontCenter", "limelight-frontc", yaw);
+            return new VisionCameraIo[] { frontRight, rear, frontLeft, frontCenter };
+        } else {
+            return new VisionCameraIo[] {};
+        }
     }
 }
